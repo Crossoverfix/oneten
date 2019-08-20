97b24fae5a011ff78c55d2e6e5ecd83793412e3c
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2019-08-20 20:05:45 +0300

    v0.92

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,4 +79,19 @@ $(document).ready(function () {
             }
         }
     }
+    var $clientControl = $('[data-js-client]');
+    var $clientContent = $('.client__list__wrap__body');
+    $clientControl.on('click',function () {
+        let $direction = $(this).attr('data-js-client');
+        if ($direction == 'up'){
+            if($clientContent.css('bottom').slice(0,-2) <= ($clientContent.outerHeight() - $('.client__list__wrap').outerHeight())){
+                $clientContent.animate({bottom: +$clientContent.css('bottom').slice(0,-2) + 100 +'px'},250);
+            }
+        } else if ($direction == 'down'){
+            if($clientContent.css('bottom').slice(0,-2) > 0){
+                $clientContent.animate({bottom: +$clientContent.css('bottom').slice(0,-2) - 100 +'px'},250);
+            }
+        }
+        return false
+    })
 })
